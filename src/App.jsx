2fe3510ffe7f930c3ab039b52f5106c8affe0b88
Samithsr <samithrgowda@gmail.com>
2025-04-10 23:53:41 +0530

    remove styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <div>
-        <h1>Hello Frontend</h1>
-      </div>
-    </>
-  );
+    <div>
+      <h1>Hello Frontend</h1>
+    </div>
+  )
 }
 
-export default App;
+export default App
